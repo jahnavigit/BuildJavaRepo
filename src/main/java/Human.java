3c281a7e7391c733750df8727b407b123d4f8fbe
jahnavigit <36572853+jahnavigit@users.noreply.github.com>
2018-02-20 16:37:04 -0600

    new release commit

--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -1,5 +1,6 @@
 public class Human {
 	public static void main(String[] args) {
 	   System.out.println("Hello Humans! Demo on Maven This is new update");
+		System.out.println("new release");
 	}
 }
